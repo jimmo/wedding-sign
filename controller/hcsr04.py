6c53b8bde3161a0e19fde6f2a3b909849335a103
Jim Mussared <jim.mussared@gmail.com>
2017-09-18 23:42:31 +1000

    Update to use much-faster memoryview.

--- controller/hcsr04.py
+++ controller/hcsr04.py
@@ -1,26 +1,23 @@
-import machine, time
+import machine, utime
 
 class HCSR04:
+  __slots__ = ['_trigger_pin', '_echo_pin', 'mm']
+
   def __init__(self, trigger_pin, echo_pin):
-    self.trigger_pin = trigger_pin
-    self.echo_pin = echo_pin
+    self._trigger_pin = trigger_pin
+    self._echo_pin = echo_pin
+    self.mm = 4000
 
-  def _echo(self):
-    self.trigger_pin.init(mode=machine.Pin.OUT, pull=None, value=0)
-    time.sleep_us(5)
-    self.trigger_pin.value(1)
-    time.sleep_us(10)
-    self.trigger_pin.value(0)
-    self.echo_pin.init(mode=machine.Pin.IN, pull=None)
-    return machine.time_pulse_us(self.echo_pin, 1, 500*2*30)
-
-  def distance_mm(self):
-    # To calculate the distance we get the pulse_time and divide it by 2
-    # (the pulse walk the distance twice) and by 29.1 becasue
-    # the sound speed on air (343.2 m/s), that It's equivalent to
-    # 0.34320 mm/us that is 1mm each 2.91us
-    # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582
-    t = self._echo()
+  @micropython.native
+  def update(self):
+    self._trigger_pin.init(mode=machine.Pin.OUT, pull=machine.Pin.PULL_NONE, value=0)
+    utime.sleep_us(5)
+    self._trigger_pin.value(1)
+    utime.sleep_us(10)
+    self._trigger_pin.value(0)
+    self._echo_pin.init(mode=machine.Pin.IN, pull=machine.Pin.PULL_NONE)
+    t = machine.time_pulse_us(self._echo_pin, 1, 500*2*30)
     if t < 0:
-      return 4000
-    return t * 100 // 582
+      self.mm = 4000
+    else:
+      self.mm = t * 3432 // 20000
